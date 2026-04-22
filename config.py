f4c8816d033794e5e28d9ec1ba34aaa23f4bb7ed
herraChron <thnxans@email.com>
2026-04-22 16:01:26 +0700

    update config

--- config.py
+++ config.py
@@ -5,10 +5,15 @@ load_dotenv()
 
 
 class PyroConf:
-    API_ID         = int(os.environ["API_ID"])
-    API_HASH       = os.environ["API_HASH"]
-    BOT_TOKEN      = os.environ["BOT_TOKEN"]
-    SESSION_STRING = os.environ["SESSION_STRING"]
-    PHONE          = os.environ["PHONE"]  
-    BOT_OWNER_ID   = int(os.environ.get("BOT_OWNER_ID", 0))  
-    WEBHOOK_URL    = os.environ.get("WEBHOOK_URL", None) 
+    API_ID           = int(os.environ["API_ID"])
+    API_HASH         = os.environ["API_HASH"]
+    BOT_TOKEN        = os.environ["BOT_TOKEN"]
+    SESSION_STRING   = os.environ["SESSION_STRING"]
+    PHONE_NUMBER     = os.environ["PHONE_NUMBER"]
+    BOT_OWNER_ID     = int(os.environ.get("BOT_OWNER_ID", 0))
+    WEBHOOK_URL      = os.environ.get("WEBHOOK_URL", None)
+    ALLOWED_USER_IDS = [
+        int(uid.strip())
+        for uid in os.environ.get("ALLOWED_USER_IDS", "").split(",")
+        if uid.strip()
+    ]
